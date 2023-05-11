0e8b801b6f94b5989ad8fae2de4a32ca716014ba
Sherry Wang <y2683wan@uwaterloo.ca>
2023-05-11 20:47:09 +1000

    init

---
+++ Whisper Auto Captions/Whisper_Auto_CaptionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Whisper_Auto_CaptionsApp.swift
+//  Whisper Auto Captions
+//
+//  Created by Sherry Wang on 30/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct Whisper_Auto_CaptionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
